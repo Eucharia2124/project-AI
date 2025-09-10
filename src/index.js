5fce6de6904c73bab9ae26d93b61838e0b7e4645
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-09-10 21:23:18 +0200

    user instruction added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function displayResearch(response) {
+ 
 
    new Typewriter('#research', {
   strings: response.data.answer,
@@ -13,19 +14,19 @@ function generateResearch(event) {
 
     let instructionInput = document.querySelector("#user-instructions");
     let apiKey = "35f4a664892ct08f69ca41oe92eb5099";
-    let prompt = `User instructions: Generate a short note about ${instructionInput.value}`;
-    let context = "You are a research expert and can help in any situation. Your mission is to generate a 4 line note in basic HTML and separete each line with a <br />. Make sure the poem in basic HTML. make sure to follow the user`s instructions. Do not include a title to the poem, Sign the poem with `SheCodes AI` inside a <strong> element";
+    let prompt = `User instructions: Generate a short answer about ${instructionInput.value}`;
+    let context = "You are a research expert and can help in any situation. Your mission is to generate a 4 line suggestions in basic HTML and separete each line with a <br />. Make sure the poem in basic HTML. make sure to follow the user`s instructions. Do not include a title to the poem, Sign the poem with `PetOby AI` inside a <strong> element";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let researchElement = document.querySelector("#research");
   researchElement.classList.remove("hidden");
-  researchElement.innerHTML = `<div class = "generating">❤ Generating a French poem about ${instructionInput.value}</div>`;
-
+  researchElement.innerHTML = `<div class = "generating">❤ Generating a research note about ${instructionInput.value}</div>`;
 
    
-    axios.get(apiUrl).then(displayresearch);
+    axios.get(apiUrl).then(displayResearch);
+   
 };
 
 
 let researchElement = document.querySelector("#research-generator-form");
-researchElementt.addEventListener("submit", generateResearch);
+researchElement.addEventListener("submit", generateResearch);

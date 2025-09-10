3c951e50193ab42191b0989cf5047f77ce520d93
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-09-10 20:39:08 +0200

    index.html, style.css and index.js added

---
+++ src/index.js
@@ -0,0 +1,31 @@
+function displayResearch(response) {
+
+   new Typewriter('#research', {
+  strings: response.data.answer,
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+}
+
+function generateResearch(event) {
+    event.preventDefault()
+
+    let instructionInput = document.querySelector("#user-instructions");
+    let apiKey = "35f4a664892ct08f69ca41oe92eb5099";
+    let prompt = `User instructions: Generate a short note about ${instructionInput.value}`;
+    let context = "You are a research expert and can help in any situation. Your mission is to generate a 4 line note in basic HTML and separete each line with a <br />. Make sure the poem in basic HTML. make sure to follow the user`s instructions. Do not include a title to the poem, Sign the poem with `SheCodes AI` inside a <strong> element";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let researchElement = document.querySelector("#research");
+  researchElement.classList.remove("hidden");
+  researchElement.innerHTML = `<div class = "generating">❤ Generating a French poem about ${instructionInput.value}</div>`;
+
+
+   
+    axios.get(apiUrl).then(displayresearch);
+};
+
+
+let researchElement = document.querySelector("#research-generator-form");
+researchElementt.addEventListener("submit", generateResearch);
